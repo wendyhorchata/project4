f5e586a66dbbdb16698f2d959eeaae8dcb3e69ff
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-07-26 19:03:21 -0700

    deploy

--- question/settings.py
+++ question/settings.py
@@ -130,3 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 
 LOGOUT_REDIRECT_URL = 'home'
+
+
+#Configure Django App for Heroku 
+import django_on_heroku
+django_on_heroku.settings(locals())
